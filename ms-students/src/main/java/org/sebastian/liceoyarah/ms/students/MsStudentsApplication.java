393d9e7d7e6cc80e408f22113673ef79e64e1ab6
Sebastian Medina <JSebastian19952011@gmail.com>
2024-08-26 21:06:09 -0500

    Se agrega la estructura inicial del MS Students para ir ampliando la aplicación

---
+++ ms-students/src/main/java/org/sebastian/liceoyarah/ms/students/MsStudentsApplication.java
@@ -0,0 +1,13 @@
+package org.sebastian.liceoyarah.ms.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStudentsApplication.class, args);
+	}
+
+}
