5320e2883e7859512567c6d83ec4b083a1a5ef04
Sebastian Medina <JSebastian19952011@gmail.com>
2024-09-14 20:57:53 -0500

    Se monta micro servicio de profesores

---
+++ ms-professors/src/main/java/com/sebastian/liceoyarah/ms/professors/MsProfessorsApplication.java
@@ -0,0 +1,13 @@
+package com.sebastian.liceoyarah.ms.professors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProfessorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProfessorsApplication.class, args);
+	}
+
+}
---
+++ ms-professors/src/test/java/com/sebastian/liceoyarah/ms/professors/MsProfessorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.liceoyarah.ms.professors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProfessorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
