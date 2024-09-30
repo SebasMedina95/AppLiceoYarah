d63749a5dfc601ba98a0ead6ba2c779197094ef2
Sebastian Medina <JSebastian19952011@gmail.com>
2024-09-29 23:52:57 -0500

    Implementación parcial de APIGW

--- apigw-liceoyarah/src/main/java/org/sebastian/liceoyarah/apigw/liceoyarah/Application.java
+++ apigw-liceoyarah/src/main/java/org/sebastian/liceoyarah/apigw/liceoyarah/Application.java
@@ -1,17 +1,15 @@
-package org.sebastian.liceoyarah.ms.persons;
+package org.sebastian.liceoyarah.apigw.liceoyarah;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.PropertySource;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@PropertySource(value = "classpath:swagger.properties", encoding = "UTF-8")
-public class MsPersonsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsPersonsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ apigw-liceoyarah/src/test/java/org/sebastian/liceoyarah/apigw/liceoyarah/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebastian.liceoyarah.apigw.liceoyarah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
